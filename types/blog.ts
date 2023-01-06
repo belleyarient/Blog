ac1a933c53744312d4f9888f2b9e7947a82deef7
shelling <belltree9712@gmail.com>
2023-01-06 22:59:25 +0900

    microCMSの導入/ブログ一覧の取得

---
+++ types/blog.ts
@@ -0,0 +1,11 @@
+export type Blog = {
+    id: string;
+    content: string;
+    title: string;
+    // tags: Tag[];
+    image: string;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+    revisedAt: string;
+  };
