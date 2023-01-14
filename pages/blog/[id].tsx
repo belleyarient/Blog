410558f48bf4b10274ba47ae1c1a1dc7adc903e9
shelling <belltree9712@gmail.com>
2023-01-14 18:07:24 +0900

    フッターの追加

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -4,6 +4,7 @@ import { Blog } from "../../types/blog";
 import "tailwindcss/tailwind.css";
 import  Head  from "./components/head";
 import { MarkdownTemplate } from "./components/markdownTemplate";
+import Footer from "./components/footer";
 
 
 type Props = {
@@ -12,9 +13,9 @@ type Props = {
 
 export default function BlogId({ blog }: Props) {
   return (
-    <main className="">
+    <main className="bg-gray-800 min-h-screen relative pb-12">
       <Head></Head>
-      <div className="bg-gray-800 p-2 pl-8 pr-8 pt-5">
+      <div className="bg-gray-800 p-2 pl-8 pr-8 pt-5 h-full">
         <h1 className="text-3xl mb-5 border-b border-red-500">{blog.title}</h1>
         <p>{blog.publishedAt}</p>
         {/* {blog.content} */}
@@ -25,6 +26,7 @@ export default function BlogId({ blog }: Props) {
         /> */}
         <MarkdownTemplate source={blog.content} />
       </div>
+      <Footer></Footer>
     </main>
   );
 }
