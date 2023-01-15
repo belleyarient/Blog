05e0a9f19c0df0b7e2a3e30ef0ea4bfcfc281607
shelling <belltree9712@gmail.com>
2023-01-15 21:59:53 +0900

    markdownの修正

--- pages/blog/components/markdownTemplate.tsx
+++ pages/blog/components/markdownTemplate.tsx
@@ -1,190 +1,232 @@
-import { BoxProps, Box, Text, UnorderedList, ListItem, Link } from '@chakra-ui/react';
-// import Markdown from 'markdown-to-jsx';
-// import { htmlToMarkdown } from '../utils/posts';
+import {
+  BoxProps,
+  Box,
+  Text,
+  UnorderedList,
+  OrderedList,
+  Heading,
+  ListItem,
+  Link,
+  Image,
+  Code as ChakraCode
+} from "@chakra-ui/react";
+
 import parse, { domToReact, HTMLReactParserOptions } from 'html-react-parser';
-// import highlight from 'highlight.js';
-// import 'highlight.js/styles/night-owl.css';
-
-
+import highlight from 'highlight.js';
+import 'highlight.js/styles/hybrid.css'
 
 type MarkdownTemplateProps = {
   source: string;
 } & BoxProps;
 
-// const ChakraMarkdown = chakra(Markdown);
-
-const headingStyle = {
-  mt: '10',
-  mb: '6',
-  fontWeight: 'semibold',
-};
-const borderBottomStyle = {
-  pb: '1',
-  borderBottom: '2px',
-  borderBottomColor: 'gray.200',
-};
-
 const h1 = {
-  component: Text,
   props: {
-    ...headingStyle,
-    ...borderBottomStyle,
-    fontSize: '3xl',
-    // as: 'h1',
+      mt: "24px",
+      mb: "16px",
+      lineHeight: "1.25",
+      fontWeight: "600",
+      pb: ".3em",
+      fontSize: "2em",
+      borderBottom: "1px solid #E7ECF2",
   },
 };
 
 const h2 = {
-  component: Text,
   props: {
-    ...headingStyle,
-    fontSize: '2xl',
-    // as: 'h2',
+      mt: "24px",
+      mb: "16px",
+      lineHeight: "1.25",
+      fontWeight: "600",
+      pb: ".3em",
+      fontSize: "1.5em",
+      borderBottom: "1px solid #E7ECF2",
   },
 };
 
 const h3 = {
-  component: Text,
   props: {
-    ...headingStyle,
-    fontSize: 'xl',
-    // as: 'h3',
+      mt: "24px",
+      mb: "16px",
+      lineHeight: "1.25",
+      fontWeight: "600",
+      fontSize: "1.25em",
   },
 };
 
 const p = {
-  component: Text,
   props: {
-    lineHeight: '180%',
-    mb: '6',
+      lineheight: "1.8",
+      mb: "10px",
+      fontSize: "18px",
+      color: "##000",
   },
 };
 
-// const code = {
-//   component: Box,
-//   props: {
-//     fontSize: 'sm',
-//     p: '4',
-//     mb: '6',
-//   },
-// };
 
-// const ul = {
-//   component: UnorderedList,
-// };
+const ul = {
+  props: {
+      my: "1",
+      lineHeight: "2",
+      pl: "1em"
+  },
+};
 
-// const li = {
-//   component: ListItem,
-// };
+const ol = {
+  props: {
+      my: "1",
+      lineHeight: "2",
+      pl: "1em"
+  }
+};
+
+const li = {
+  props: {
+      fontSize: "18px"
+  },
+};
 
 const blockquote = {
-  component: Box,
   props: {
-    borderLeft: '2px',
-    pl: '4',
-    mb: '6',
-    color: 'gray.500',
-    // as: 'blockquote',
-  },
-};
+      color: "gray.500",
+      my: "1em",
+      pl: "2em",
+      borderLeft: '2px',
+      borderColor: 'gray.500',
+      fontSize: "18px",
+      lineHeight: "1.8",
+  }
+}
 
 const a = {
-  component: Link,
   props: {
-    isExternal: true,
-    textDecoration: 'underline',
+      isExternal: true,
+      textDecoration: "none",
+      color: "#3182CE",
+      _hover: {
+          textDecoration: "none",
+          color: "#4593e6",
+      },
   },
 };
 
-// const languageSubset = ['js', 'html', 'css', 'xml', 'typescript'];
+const img = {
+  props: {
+      border: "1px",
+      borderColor: "gray.300"
+  }
+}
+
+const code = {
+  props: {
+      fontSize: 'md',
+      px: "0.2em",
+      mx: "0.2rem",
+  },
+}
+
+const preCode = {
+  props: {
+      fontSize: "18px",
+  }
+}
+
+const languageSubset = ['js', 'html', 'css', 'xml', 'typescript', 'python'];
 
 const options: HTMLReactParserOptions = {
   replace: (domNode: any) => {
-    if (domNode.type === 'tag') {
-      if (domNode.name === 'h1') {
-        return (
-          <Text as="h1" {...h1.props}>
-            {domToReact(domNode.children, options)}
-          </Text>
-        );
+      if (domNode.type === "tag") {
+          if (domNode.name === "h1") {
+              return (
+                  <Heading as="h1" {...h1.props}>
+                      {domToReact(domNode.children, options)}
+                  </Heading>
+              );
+          }
+          if (domNode.name === "h2") {
+              return (
+                  <Heading className="text-blue-300" as="h2" {...h2.props}>
+                      {domToReact(domNode.children, options)}
+                  </Heading>
+              );
+          }
+          if (domNode.name === "h3") {
+              return (
+                  <Text as="h3" {...h3.props}>
+                      {domToReact(domNode.children, options)}
+                  </Text>
+              );
+          }
+          if (domNode.name === "ul") {
+              return (
+                  <UnorderedList {...ul.props}>
+                      {domToReact(domNode.children, options)}
+                  </UnorderedList>
+              );
+          }
+          if (domNode.name === 'ol') {
+              return (
+                  <OrderedList {...ol.props}>
+                      {domToReact(domNode.children, options)}
+                  </OrderedList>
+              )
+          }
+          if (domNode.name === "li") {
+              return (
+                  <ListItem {...li.props}>
+                      {domToReact(domNode.children, options)}
+                  </ListItem>
+              )
+          }
+          if (domNode.name === "p") {
+              return (
+                  <Text {...p.props}>{domToReact(domNode.children, options)}</Text>
+              );
+          }
+          if (domNode.name === 'blockquote') {
+              return (
+                  <Box {...blockquote.props}>
+                      {domToReact(domNode.children, options)}
+                  </Box>
+              )
+          }
+          if (domNode.name === "a") {
+              return (
+                  <Link {...a.props} href={domNode.attribs.href}>
+                      {domToReact(domNode.children, options)}
+                  </Link>
+              );
+          }
+          if (domNode.name === 'img') {
+              return (
+                  <Image {...img.props} src={domNode.attribs.src} />
+              )
+          }
+          if (domNode.name === 'code') {
+              // 通常のcodeタグとpre→codeタグを区別する
+              if (domNode.parent.name === 'pre') {
+                  const highlightCode = highlight.highlightAuto(
+                      domToReact(domNode.children) as string,
+                      languageSubset,
+                  ).value;
+                  return (
+                      <Box as="code" className="hljs" {...preCode.props}>
+                          {parse(highlightCode)}
+                      </Box>
+                  );
+              } else {
+                  return (
+                      <ChakraCode {...code.props}>
+                          {domToReact(domNode.children, options)}
+                      </ChakraCode>
+                  )
+              }
+          }
       }
-      if (domNode.name === 'h2') {
-        return (
-          <Text className='text-blue-200 border-b text-xl pt-4' as="h2" {...h2.props}>
-            {domToReact(domNode.children, options)}
-          </Text>
-        );
-      }
-      if (domNode.name === 'h3') {
-        return (
-          <Text as="h3" {...h3.props}>
-            {domToReact(domNode.children, options)}
-          </Text>
-        );
-      }
-      if (domNode.name === 'ul') {
-        return <UnorderedList>{domToReact(domNode.children, options)}</UnorderedList>;
-      }
-      if (domNode.name === 'li') {
-        return <ListItem>{domToReact(domNode.children, options)}</ListItem>;
-      }
-      if (domNode.name === 'blockquote') {
-        return (
-          <Box as="blockquote" {...blockquote.props}>
-            {domToReact(domNode.children, options)}
-          </Box>
-        );
-      }
-      // if (domNode.name === 'code') {
-      //   const highlightCode = highlight.highlightAuto(
-      //     domToReact(domNode.children) as string,
-      //     languageSubset,
-      //   ).value;
-      //   return (
-      //     <Box as="code" className="hljs" {...code.props}>
-      //       {parse(highlightCode)}
-      //     </Box>
-      //   );
-      // }
-      if (domNode.name === 'a') {
-        return (
-          <Link {...a.props} href={domNode.attribs.href}>
-            {domToReact(domNode.children, options)}
-          </Link>
-        );
-      }
-      if (domNode.name === 'p') {
-        return <Text {...p.props}>{domToReact(domNode.children, options)}</Text>;
-      }
-    }
-  },
-};
+  }
+}
 
  const MarkdownTemplate = (props: MarkdownTemplateProps) => {
-  return (
-    
-
-      <Box {...props}>{parse(props.source, options)}</Box>
-    
-    // <ChakraMarkdown
-    //   lineHeight="tall"
-    //   options={{
-    //     wrapper: 'article',
-    //     overrides: {
-    //       h1,
-    //       h2,
-    //       h3,
-    //       p,
-    //       ul,
-    //       li,
-    //       blockquote,
-    //       a,
-    //     },
-    //   }}
-    //   {...props}
-    // >
-    //   {props.source}
-    // </ChakraMarkdown>
-  );
+  return <Box {...props}>{parse(props.source, options)}</Box>;
 };
+
 export default MarkdownTemplate;
